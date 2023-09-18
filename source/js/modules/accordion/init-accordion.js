67c01728ccafe5a8f3d841c9614a2b80047c539b
Misha074 <103427594+Misha074@users.noreply.github.com>
2023-09-19 00:30:04 +0400

    Много поработал
    А впереди ещё больше

--- source/js/modules/accordion/init-accordion.js
+++ source/js/modules/accordion/init-accordion.js
@@ -1,5 +1,12 @@
 import {Accordions} from './accordions';
 let accordions;
+// Находим все контенты которые раскрыты в случае неподключения скрипта.
+let accordionContent = document.querySelectorAll('.accordion__content--nojs');
+
+for (let i = 0; i < accordionContent.length; i++) {
+  // Находим все раскрытые контенты и удаляем класс который ответсвенен за их раскрытие.
+  accordionContent[i].classList.remove('accordion__content--nojs');
+}
 
 const initAccordions = () => {
   accordions = new Accordions();
